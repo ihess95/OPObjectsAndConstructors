195bd285d9e21c2fddff1304635f59c1791d641f
Ian Hess <ian.h.hess@gmail.com>
2022-12-13 14:19:39 -0500

    start tutorial

---
+++ app.js
@@ -0,0 +1,16 @@
+//example one
+const playerOneName = "tim";
+const playerTwoName = "jenn";
+const playerOneMarker = "X";
+const playerTwoMarker = "O";
+
+//example two
+const playerOne = {
+  name: "tim",
+  Marker: "x",
+};
+
+const playerTwo = {
+  name: "jenn",
+  marker: "O",
+};
