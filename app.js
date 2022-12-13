4f89a819b6f363462b4eda5feb3736eb7b130c14
Ian Hess <ian.h.hess@gmail.com>
2022-12-13 14:38:22 -0500

    run constructor

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-//example one
-const playerOneName = "tim";
-const playerTwoName = "jenn";
-const playerOneMarker = "X";
-const playerTwoMarker = "O";
-
-//example two
-const playerOne = {
-  name: "tim",
-  Marker: "x",
-};
-
-const playerTwo = {
-  name: "jenn",
-  marker: "O",
-};
-
-function Player(name, marker) {
+function Book(name, author, pageCount, read) {
   this.name = name;
-  this.marker = marker;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.read = read;
+  this.info = function () {
+    console.log(
+      `${name} by ${author}, ${pageCount} pages, ${
+        read === true ? "has been read." : "not read yet."
+      }`
+    );
+  };
 }
 
-const player = new Player("steve", "x");
-console.log(player.name); // 'steve'
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", true);
+console.log(theHobbit.info());
